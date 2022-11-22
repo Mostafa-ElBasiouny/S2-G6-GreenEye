9ef1acd6ed2efa001f6c548456591961ff4beed2
Mostafa <91155192+Mostafa-ElBasiouny@users.noreply.github.com>
2022-11-21 21:52:29 -0500

    Add unit test solution.

---
+++ S2-G6-GreenEye-UT/S2-G6-GreenEye-UT.cpp
@@ -0,0 +1,79 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace S2G6GreenEyeUT
+{
+	TEST_CLASS(Database)
+	{
+	public:
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+
+	TEST_CLASS(PH)
+	{
+	public:
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+
+	TEST_CLASS(CO2)
+	{
+	public:
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+
+	TEST_CLASS(UVIndex)
+	{
+	public:
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+
+	TEST_CLASS(Temperature)
+	{
+	public:
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+
+	TEST_CLASS(AirHumidity)
+	{
+	public:
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+
+	TEST_CLASS(SoilMoisture)
+	{
+	public:
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+
+	TEST_CLASS(Precipitation)
+	{
+	public:
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+
+	TEST_CLASS(SoilFertility)
+	{
+	public:
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
